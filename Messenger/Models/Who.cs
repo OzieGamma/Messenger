39b431e76d655558edf728ed6664388699eddc46
OzieGamma (Oswald Maskens) <ozie@live.be>
2014-10-04 18:31:09 +0200

    Added who route

---
+++ Messenger/Models/Who.cs
@@ -0,0 +1,32 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Who.cs" company="Oswald Maskens">
+//   Copyright 2014 Oswald Maskens
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//   
+//       http://www.apache.org/licenses/LICENSE-2.0
+//   
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Messenger.Models
+{
+    using System.Configuration;
+
+    internal static class Who
+    {
+        internal static string AmI
+        {
+            get
+            {
+                return ConfigurationManager.AppSettings["UID"];
+            }
+        }
+    }
+}
