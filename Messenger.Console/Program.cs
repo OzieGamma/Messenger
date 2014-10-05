a655a0a44c026d14ea876f9805bbadb606749748
OzieGamma (Oswald Maskens) <ozie@live.be>
2014-10-05 20:03:22 +0200

    modified program to sign off this project !

--- Messenger.Console/Program.cs
+++ Messenger.Console/Program.cs
@@ -19,7 +19,6 @@ namespace Messenger.Console
 {
     using System;
     using System.Collections.Generic;
-    using System.IO;
     using System.Linq;
     using System.Threading.Tasks;
 
@@ -31,65 +30,23 @@ namespace Messenger.Console
         {
             DefineMy();
 
-            var fromFile = File.ReadAllLines("input.inp").Where(_ => !_.StartsWith("#"));
-            var enumerable = fromFile as IList<string> ?? fromFile.ToList();
+            var recipients = new string[0]; // define who will recieve your message
 
-            var phoneNumbers = enumerable.Where(_ => _.StartsWith("+")).ToList();
-            var emailRecipients = enumerable.Where(_ => _.Contains("@")).Where(_ => !string.IsNullOrWhiteSpace(_));
-
-            var emails =
-                emailRecipients.Select(
+            var transfers =
+                recipients.Select(
                     _ =>
                     new TransferRequest
                     {
                         FinalProtocol = TransferRequestProtocol.Email, 
                         FinalTo = _, 
-                        Payload = @"This is my ap for the 2014 fall Jackobshack hackathon. 
-                        I think azure is cool and to think I can send messages in the cloud for free is amazing. 
-                        I highly recommend http://azure.microsoft.com/. 
-                        All the code for my project is released under the apache2 license(http://www.apache.org/licenses/LICENSE-2.0.html) and can be found at https://github.com/oziegamma. 
-                        If you are ever interested in a project / creating a company contact me at oswald@maskens.eu, I'm always up for a geeky adventure. 
-                        I hope you all had a nice day. I had a lot of fun playing with VM-farms", 
+                        Payload = @"Lorem ipsum", // define your message 
                         RedPill = MyRandom.RedPill(100, 5000), 
                         ShouldStamp = true, 
                         Trace = new List<string>()
                     });
-            var smses =
-                phoneNumbers.Select(
-                    _ =>
-                    new TransferRequest
-                    {
-                        FinalProtocol = TransferRequestProtocol.Sms, 
-                        FinalTo = _, 
-                        Payload =
-                            @"OzieGamma's App for Jackobshack 2014. Let's keep in touch at oswald@maskens.eu", 
-                        RedPill = MyRandom.RedPill(2, 2), 
-                        ShouldStamp = false, 
-                        Trace = new List<string>()
-                    });
 
-            var phoneCalls =
-                phoneNumbers.Select(
-                    _ =>
-                    new TransferRequest
-                    {
-                        FinalProtocol = TransferRequestProtocol.Call, 
-                        FinalTo = _, 
-                        Payload =
-                            @"Hey there, this is oswald maskens, I'm calling to tell you this is my App for Jackobshack 2014. 
-                            I hope you had a good time, I certainly did. Don't hesitate to contact me if you have a cool project ! 
-                            The easiest way to reach me is email: oswald@maskens.eu. 
-                            You can also find me on Facebook, GitHub, Stackoverflow and LinkedIn.", 
-                        RedPill = MyRandom.RedPill(2, 2), 
-                        ShouldStamp = false, 
-                        Trace = new List<string>()
-                    });
-
-            var transfers = new List<TransferRequest>(emails.ToList());
-            transfers.AddRange(smses);
-            transfers.AddRange(phoneCalls);
-
-            Parallel.ForEach(transfers, _ => new TransferClient().Transfer(_));
+            // Run it !!!
+            Parallel.ForEach(transfers, SendRequest);
 
             Console.Read();
         }
